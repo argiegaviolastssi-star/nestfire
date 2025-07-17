dda80173801e9df57340719b1b5743bcf26ba6c4
FelipeOsano <felipeosano21@hotmail.com>
2025-07-17 16:09:13 -0300

    Refactor imports and update Firebase Functions version
    - Updated imports in index.ts to include Firebase Admin and export it.
    - Changed import paths in trigger.interface.ts and event-trigger.ts to use 'firebase-functions/v1' for better compatibility with the latest Firebase Functions version.

--- src/interfaces/trigger.interface.ts
+++ src/interfaces/trigger.interface.ts
@@ -1,4 +1,4 @@
-import { Change, CloudFunction, firestore } from 'firebase-functions';
+import { Change, CloudFunction, firestore } from 'firebase-functions/v1';
 
 export interface ITrigger {
   [key: string]: CloudFunction<Change<firestore.DocumentSnapshot>>;
